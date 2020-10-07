9c2da509dd74605c86e38cb4f0bf14d5913ccb41
anushavp <44489560+anushavp@users.noreply.github.com>
2020-10-07 11:37:57 +0530

    Add files via upload

---
+++ cuisine/connect.php
@@ -0,0 +1,14 @@
+﻿
+ <?php 
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "Indian";
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	$sql = "INSERT INTO `food`(`name`, `email`, `message`) VALUES ([value-1],[value-2],[value-3])";
+	$result = $conn->commit($sql);
+	
+?>
